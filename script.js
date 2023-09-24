9aa0e604a6890eb3e100967c80352c57f00c4d37
asc2563 <120886940+asc2563@users.noreply.github.com>
2023-09-24 10:56:54 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+var ifrm = document.createElement("iframe");
+function slope() {
+  ifrm.setAttribute("src", "https://bonbang.github.io/slope/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function geometrydash() {
+  ifrm.setAttribute("src", "https://bonbang.github.io/geometry-dash/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function redball4() {
+  ifrm.setAttribute("src", "https://techgai.github.io/red-ball-4/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function cuttherope() {
+  ifrm.setAttribute("src", "https://techgai.github.io/cut-the-rope/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function subway_surfers() {
+  ifrm.setAttribute("src", "https://bonbang.github.io/subway-surfers/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function minesweeper() {
+  ifrm.setAttribute("src", "https://techgai.github.io/minesweeper/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function minecraft() {
+  ifrm.setAttribute("src", "https://codehay.net/gm/minecraft-1.8/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function tfe() {
+  ifrm.setAttribute("src", "https://topvazstore.github.io/g/2048/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function bird() {
+  ifrm.setAttribute("src", "https://techgai.github.io/flappy-bird/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function cc() {
+  ifrm.setAttribute("src", "https://topvazstore.github.io/g/cookie-clicker/");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);
+}
+function roblox() {
+  ifrm.setAttribute("src", "https://beta7.apktbg.com/uv/education/hvtrs8%2F-nmw%2Cge%2Frlcy-rmbnoz-aoppmrctkol%2F7369-rmbnoz");
+  ifrm.style.width = "100%";
+  ifrm.style.height = "750px";
+  document.body.appendChild(ifrm);}
+function emtpy() {
+  
+window.location.reload();
+}
+function discord() {
+  window.open('https://discord.gg/Jeybsy2us4')
+}
